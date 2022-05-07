e4bc3d0a413b3542a821366f1607b608fa0a41e0
kunal Jha <kunaljha331@gmail.com>
2022-05-07 08:18:17 +0530

    water

--- Hungry.py
+++ Hungry.py
@@ -3,4 +3,7 @@ if hungry=='yes':
     print('eat biryani')
     print('pizza')
 else:
+    thirsty=input('are u thirsty?')
+    if thirsty=='yes':
+        print('drink water')
     print('do your work')    
